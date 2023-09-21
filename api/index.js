1a088b72c78b8637ebfa1add75e1867f1c1ae588
mayank ekaghara <mayankekaghara.me@gmail.com>
2023-09-21 14:40:51 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Check for database connection errors
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 const app = express();
 
